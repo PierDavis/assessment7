00d27dc1914e472cc63ea0e3ff90cfc058e93abb
Pier Davis <pieramelia2@gmail.com>
2022-01-14 12:06:43 -0500

    runtime

--- steptwo.js
+++ steptwo.js
@@ -3,6 +3,9 @@
    
 // }
 
+//runtime guess: '16.317 μs'
+
+
 function unique(string) {
     for(let i = 0; i < string.length; i++)
     for(let h = i + 1; h < string.length; h++)
@@ -10,11 +13,15 @@ function unique(string) {
             return false;
     return true;
 }
+//runtime guess: '16.317 μs'
+
 
 let Pangram = (string) =>
     new Set(string.toLowerCase().match(/[a-z]/g)).size ===26;
 
 console.log(Pangram("The Quick Brown Fox Jumps Over The Lazy Dog"));
+//runtime guess: '16.317 μs'
+
 
 function find_longest_word(string) {
     let array = string.match(/\w[a-z]{0,}/gi);
@@ -28,3 +35,5 @@ function find_longest_word(string) {
     return result;
 }
 console.log(find_longest_word('This is a string'));
+
+//runtime guess: '16.317 μs'
