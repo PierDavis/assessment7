13a295862ba242554d64b2a85d955a4cb5ff3ac0
Pier Davis <pieramelia2@gmail.com>
2022-01-14 11:56:46 -0500

    steptwo

--- steptwo.js
+++ steptwo.js
@@ -10,3 +10,21 @@ function unique(string) {
             return false;
     return true;
 }
+
+let Pangram = (string) =>
+    new Set(string.toLowerCase().match(/[a-z]/g)).size ===26;
+
+console.log(Pangram("The Quick Brown Fox Jumps Over The Lazy Dog"));
+
+function find_longest_word(string) {
+    let array = string.match(/\w[a-z]{0,}/gi);
+    let result = array[0];
+
+    for(let x = 1; x < array.length; x++) {
+        if(result.length < array[x].length){
+        result = array[x];
+        }
+    }
+    return result;
+}
+console.log(find_longest_word('This is a string'));
